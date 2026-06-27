4627eb0414b9fa1a014f8b0904c2d3ed98c3a3fa
shahjalal-mahmud <mahmud.nubtk@gmail.com>
2026-06-28 00:27:31 +0600

    feat: reorganize Footer component into layout directory for improved structure

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,7 +1,7 @@
 import { Outlet } from 'react-router-dom';
 import Navbar from '../components/layout/Navbar';
 import BottomNav from '../components/layout/BottomNav';
-import Footer from '../components/Footer';
+import Footer from '../components/layout/Footer';
 
 const MainLayout = () => {
   return (
