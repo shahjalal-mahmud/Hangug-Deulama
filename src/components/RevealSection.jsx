4412d7d6faf2c60ee0e759e0eb1c063aefcfe826
shahjalal-mahmud <mahmud.nubtk@gmail.com>
2026-06-28 00:13:20 +0600

    feat: add RevealSection component for scroll-based reveal animations

---
+++ src/components/RevealSection.jsx
@@ -0,0 +1,19 @@
+import useScrollReveal from '../hooks/useScrollReveal';
+
+const RevealSection = ({ as: Tag = 'div', className = '', children, ...rest }) => {
+  const { ref, isVisible } = useScrollReveal();
+
+  return (
+    <Tag
+      ref={ref}
+      className={`transition-all duration-700 ease-cinematic will-change-transform
+        ${isVisible ? 'opacity-100 translate-y-0' : 'opacity-0 translate-y-8'}
+        ${className}`}
+      {...rest}
+    >
+      {children}
+    </Tag>
+  );
+};
+
+export default RevealSection;
