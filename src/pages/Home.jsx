4a7b109e5c77d7a9fa37993ba69f455cc1d88309
shahjalal-mahmud <mahmud.nubtk@gmail.com>
2026-06-28 00:30:03 +0600

    feat: move GenrePills component to home directory for improved organization

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,7 @@
 import { useMemo, useState } from 'react';
 import { useDrama } from '../context/DramaContext';
 import HeroSection from '../components/home/HeroSection';
-import GenrePills from '../components/GenrePills';
+import GenrePills from '../components/home/GenrePills';
 import ContinueWatching from '../components/ContinueWatching';
 import TrendingSection from '../components/TrendingSection';
 import RecommendationSection from '../components/RecommendationSection';
