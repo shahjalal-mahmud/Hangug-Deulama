dbfd35830b78520f5a866dbe3ad8667e624acd7b
shahjalal-mahmud <mahmud.nubtk@gmail.com>
2026-06-28 00:38:39 +0600

    fix: remove unnecessary blank lines in MainLayout component for cleaner code

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -7,11 +7,9 @@ const MainLayout = () => {
   return (
     <div className="min-h-screen bg-background flex flex-col">
       <Navbar />
-
       <main className="flex-1 pt-24 md:pt-28 pb-28 md:pb-16">
         <Outlet />
       </main>
-
       <Footer />
       <BottomNav />
     </div>
