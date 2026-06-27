7b537e69afeaef665b0fbcc4fabbe4fa3ceeb230
shahjalal-mahmud <mahmud.nubtk@gmail.com>
2026-06-28 00:28:04 +0600

    feat: relocate NotificationButton component to layout directory for improved organization

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -2,7 +2,7 @@
 import { NavLink } from 'react-router-dom';
 import clsx from 'clsx';
 import SearchBar from '../SearchBar';
-import NotificationButton from '../NotificationButton';
+import NotificationButton from './NotificationButton';
 import ProfileMenu from '../ProfileMenu';
 
 const navLinks = [
